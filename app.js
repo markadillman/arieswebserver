687926151e6fd847483f8e7501e5cff2061891ff
markadillman <dillmanm@oregonstate.edu>
2017-04-30 21:45:13 -0400

    huh

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var xmlparser = require('express-xml-parser');
 var stylus = require('stylus');
 var SVG = require('svg.js');
 
